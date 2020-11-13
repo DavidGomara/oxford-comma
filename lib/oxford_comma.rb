2256ce8b647d6ac38f7d9266f122095ca875db3a
David Gomara <davidgomara7@gmail.com>
2020-11-13 00:34:58 +0000

    commit tree 56c5c080b78b448fbd4f8bf18d4ed8b38932a050

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+        var = array[2]
+        array.pop
+        array.push("and " + var)
+        array.join(", ")
+    elsif array.length > 3
+        var = array.last
+        array.pop
+        array.push("and " + var)
+        array.join(", ")
+    end 
+end 
